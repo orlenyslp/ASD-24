6b807ed401df36ca0760207cc8c3e92e1b2cf644
David Chapela de la Campa <david.chapela.delacampa@gmail.com>
2024-10-19 23:39:41 +0200

    [HW3] Part 1: Testing the controllers

--- takso/lib/takso_web/controllers/booking_controller.ex
+++ takso/lib/takso_web/controllers/booking_controller.ex
@@ -1,7 +1,24 @@
 defmodule TaksoWeb.BookingController do
   use TaksoWeb, :controller
 
+  # We added this function to handle the GET query to "/bookings/new" as
+  # specified by the router. If you run "mix phx.routes" you'll see that
+  # a GET query to "/bookings/new" get handled by "BookingController"
+  # through the method ":new", i.e., this function.
   def new(conn, _params) do
+    # Redirect to template "new.html"
     render conn, "new.html"
   end
+
+  # Same than before but handling the query GET to "/bookings" (":index").
+  def index(conn, _params) do
+    render conn, "index.html"
+  end
+
+  def create(conn, _params) do
+    conn
+      |> put_flash(:info, "Your taxi will arrive in 15 minutes.")
+      |> redirect(to: ~p"/bookings")
+  end
+
 end
